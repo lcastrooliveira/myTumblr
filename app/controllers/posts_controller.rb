56e6184e7b102e8012d34256a5fa41f2409bc3df
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-03-26 11:43:41 -0300

    Edit and Delete Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
@@ -21,10 +23,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title,:body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title,:body)
   end
+
+
 
 end
